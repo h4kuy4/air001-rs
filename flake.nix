4bff85e1a4888d4658c90ebf3786043666682e5a
简 <2447746158@qq.com>
2023-12-28 00:45:10 +0800

    update description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-    description = "A Nix-flake-based development environment for Kage tunnel";
+    description = "A development environment for Rust on Air001";
 
     inputs = {
         nixpkgs.url      = "github:NixOS/nixpkgs/nixos-23.11";
