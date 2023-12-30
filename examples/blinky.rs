a78f471664d1c682e7abb77bc5c2558cb76e01c1
hakuya <me@hakuya.moe>
2023-12-30 20:17:36 +0800

    Style(example): Modified some code.

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,20 +1,30 @@
 #![no_main]
 #![no_std]
 
-use air001_rs::{entry, gpio::{Port, Output, GpioBuilder}, rcc::RccBuilder, timer::systick::{ClockSource, SysTickBuilder}};
+use air001_rs::{
+    entry, 
+    gpio::{
+        Port, 
+        Output, 
+        GpioBuilder
+    }, 
+    rcc::RccBuilder, 
+    timer::systick::{
+        ClockSource, 
+        SysTickBuilder
+    }
+};
 
 #[entry]
 fn main() -> ! {
+    let _rcc = RccBuilder::new()
+        .enable_port(Port::GPIOB)
+        .build();
+
     let mut sys_tick = SysTickBuilder::new()
         .source(ClockSource::AHBDiv8)
         .disable_int()
         .set_reload(8_000)
-        .build();
-
-    sys_tick.enable();
-
-    let _rcc = RccBuilder::new()
-        .enable_port(Port::GPIOB)
         .build();
 
     let mut led = GpioBuilder::<Output, 0>::new(Port::GPIOB)
